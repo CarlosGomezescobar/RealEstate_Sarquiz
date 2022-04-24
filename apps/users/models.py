bbc7e40e4b96751cbedc21c20cf05f70760b9f2e
Sarquiz <gruposarqui@gmail.com>
2022-04-24 06:56:34 -0400

    Desarollo_Standard

---
+++ apps/users/models.py
@@ -0,0 +1,38 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+class User(AbstractUser, PermissionsMixin):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    username = models.CharField(verbose_name=_("Username"), max_length=225, unique=True)
+    first_name = models.CharField(verbose_name=_("First Name"), max_length=50, unique=True)
+    last_name = models.CharField(verbose_name=_("Last Name"), max_length=50, unique=True)
+    email = models.EmailField(verbose_name=_("Email Address"), max_length=50, unique=True)
+    if_staff = models.BooleanField(default=False)
+    if_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=["username","first_name","last_name"]
+
+    objects = CustomUserManager()
+    
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+        
+    def __str__(self):
+        return self.username
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name.title()}- {self.last_name.title()}"
+
+    def get_short_name(self):
+        return self.username 
+
+
